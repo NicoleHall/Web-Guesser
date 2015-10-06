5716cfd936a040d415d1ce81a0fc7d881ab9bfcd
Nicole Hall <nicolealexandrahall@gmail.com>
2015-10-06 15:52:55 -0600

    it is somewhat functional, there is no way too high or way too low messages

--- web_guesser.rb
+++ web_guesser.rb
@@ -1,9 +1,20 @@
 require 'sinatra'
 require 'sinatra/reloader'
 
-static = Random.new.rand(100)
-
 get '/' do
-  erb :index, :locals => {:static => static}
+  guess = params["guess"]
+  message = check_guess(guess)
+  erb :index, :locals => {:static => STATIC, :guess => guess, :message => message}
+end
 
+STATIC = Random.new.rand(100)
+
+def check_guess(guess)
+  if guess.to_i > STATIC
+    "#{guess} is too high"
+  elsif guess.to_i < STATIC
+    "#{guess} is too low"
+  elsif guess.to_i == STATIC
+    "Correct!"
+  end
 end
